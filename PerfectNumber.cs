34d692fa9e89de1ed42c1d1e23bde35a137c525d
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-21 19:50:49 +0530

    Added:Fibonacci series and Perfect number

---
+++ PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LogicalPrograms_Day6
+{
+    public class PerfectNumber
+    {
+        public static void DisplayPerfectNumber()
+        {
+
+            int sum = 0;
+            Console.WriteLine("Enter the number :-");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+
+                int reminder = number % i;
+                if (reminder == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == number)
+            {
+                // Console.WriteLine("{0}", number);
+                Console.WriteLine("number is perfect ");
+            }
+            else
+            {
+                Console.WriteLine("number is not perfect number");
+            }
+
+
+
+
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Fibo.createFibo(5);
+            //Fibo.createFibo(5);
+            PerfectNumber.DisplayPerfectNumber();
         }
     }
 }
